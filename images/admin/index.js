17ddf90b60fd1e05018b04d5f782750d15eaa01a
waqar246 <waqarg246@gmail.com>
2023-09-07 21:12:48 +0500

    Push to reositry

---
+++ images/admin/index.js
@@ -0,0 +1,17 @@
+// Inside ../../assets/Admin/index.js or ../../assets/Admin/index.ts
+export { default as notifcationIcon } from './notificationIcon.svg';
+export {default as adminWhiteLogo} from './SkinClubLogo_Logotype_White.svg'
+export {default as notificationIcon} from './notofication-icon.svg'
+export {default as sidMenuIcon} from './menu-icon.svg'
+export {default as hamburger} from './humberger.png'
+export {default as serviceImg} from './service-img.png'
+export {default as pdfIcon} from './pdf-icon.svg'
+export {default as deleteIcon} from './delete-icon.svg'
+
+
+
+
+
+
+
+// Export other assets as needed
